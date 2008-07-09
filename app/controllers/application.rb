46d0dd8fa407bebe079b174b4d00a4fd69206127
Jeff Paquette <jeff@snowmoonsoftware.com>
2008-07-09 02:02:04 +0000

    Initial pass at authentication

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,10 +2,13 @@
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   include TranslationGet
+  include AuthenticatedSystem
 
   before_filter do |c|
     ActiveRecord::Base.lang = c.session[:lang] ||= "fr"
   end
+  # require authenticated user for all actions (have to exclude signup and login right?)
+  before_filter :login_required
 
   def debug_log(text)
     logger.log 0, "####################\n#{text}\n####################"
