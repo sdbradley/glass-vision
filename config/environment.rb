5133d012bf5a27d94ad65bbcd2236ed097567ae8
Jeff Paquette <jeff@snowmoonsoftware.com>
2008-09-21 01:32:20 +0000

    fixed missing end

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,11 @@ ENV['RAILS_ENV'] ||= 'production'
 #RAILS_ENV = ENV['RAILS_ENV']
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-if `hostname`.strip == 'quotation.glass-vision.net'
+if `hostname`.strip =~ 'glass-vision'
   RAILS_GEM_VERSION = '1.2.5'
 else
   RAILS_GEM_VERSION = '1.2.6'
+end
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
