0c0e049ee487b85997a0175f0267824dcb0ab7cc
Jeff Paquette <jeff@snowmoonsoftware.com>
2008-10-15 02:59:02 +0000

    Reduced size of opening by 25 pixels Added "View from outside" below image

---
+++ db/migrate/1015_add_view_from_outside_translations.rb
@@ -0,0 +1,13 @@
+class AddViewFromOutsideTranslations < ActiveRecord::Migration
+  def self.up
+ 	execute(INSERT INTO `translations` VALUES (383, 'MSG_VIEW_FROM_OUTSIDE', 'Vue de l'extérieur', '', ' View from outside'));
+ end
+
+  def self.down
+    if RAILS_ENV == 'production'
+      raise ActiveRecord::IrreversibleMigration, "Can't remove existing translations because they may have been modified by the user"
+    else
+      execute 'delete from translations where id = 383;'
+    end
+  end
+end
