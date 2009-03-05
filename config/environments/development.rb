8bd2d0d5a6fe19525ad02875e57643acc09296bf
Jeff Paquette <jeff@snowmoonsoftware.com>
2009-03-05 03:55:40 +0000

    Added the ability to create option categories and assign options to them.
    Options cannot be in more than one category, though the database model allows that.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ config.action_controller.perform_caching             = false
 config.action_view.cache_template_extensions         = false
 config.action_view.debug_rjs                         = true
 
+#Dependencies.log_activity = true
+
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 ActionMailer::Base.delivery_method = :smtp
---
+++ db/migrate/1027_create_option_categories_association_table.rb
@@ -0,0 +1,13 @@
+class CreateOptionCategoriesAssociationTable < ActiveRecord::Migration
+  def self.up
+    create_table :option_categories_options do |t|
+        t.column :option_category_id, :integer
+        t.column :option_id, :integer
+    end
+    remove_column :option_categories_options, :id 
+  end
+
+  def self.down
+    drop_table :option_categories_options
+  end
+end
