15b38ee97724f6a4dd1fc17d385c808e5ca57be6
Jeff Paquette <jeff@snowmoonsoftware.com>
2009-05-29 18:27:14 +0000

    pass quotation to view

--- app/models/quotation_mailer.rb
+++ app/models/quotation_mailer.rb
@@ -6,7 +6,7 @@ class QuotationMailer < ActionMailer::Base
     recipients    @admin_user.email
     from          "info@glass-vision.net"
     subject       "[Glass Vision] #{trn_get('SUBJECT_NEW_QUOTATION_CREATED')}"
-    body          :user => quotation.user, :admin_user => @admin_user,
+    body          :user => quotation.user, :admin_user => @admin_user, :quotation => quotation,
                   :url => url_for(:host => "quotations.glass-vision.net", :controller => "quotation", :action => "show", :id => quotation.id)
     
     content_type = "text/html"
