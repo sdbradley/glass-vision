7578596bb9b463f0c9b95cad5dd55c4b4f380002
Jeff Paquette <jeff@snowmoonsoftware.com>
2009-11-18 03:25:33 +0000

    changed validation of hex colors

--- app/models/product_color.rb
+++ app/models/product_color.rb
@@ -2,7 +2,7 @@ class ProductColor < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_presence_of :value
-  validates_format_of :color, :with => /#[0-9a-f]{3,6}/i,  :message => "should be hex encoded (eg '#cccccc', like in CSS)", :allow_blank => true
+  validates_format_of :color, :with => /#[0-9a-f]{6}/i,  :message => "should be hex encoded (eg '#cccccc', like in CSS)", :allow_blank => true
 
 
   def color
