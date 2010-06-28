8ef75cfc117d8fa233a99014c25ddb2bbc3daf06
Jeff Paquette <jeff@snowmoonsoftware.com>
2010-06-28 18:07:32 +0000

    Add qty disclaimer for custom colors

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100628123618) do
+ActiveRecord::Schema.define(:version => 20100628175741) do
 
   create_table "companies", :force => true do |t|
     t.string  "name",              :limit => 100, :default => "", :null => false
