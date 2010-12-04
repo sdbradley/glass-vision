8d26f1926c2f02b4cb00d05e51e2418b99696a65
Jeff Paquette <jeff@snowmoonsoftware.com>
2010-12-04 14:53:58 +0000

    removed Bullet

--- config/environment.rb
+++ config/environment.rb
@@ -31,8 +31,6 @@ Rails::Initializer.run do |config|
 
   config.gem 'paperclip',  :source => 'http://gemcutter.org'
   config.gem 'formtastic', :source => 'http://gemcutter.org'
-#  config.gem "ruby-growl", :source => "http://gemcutter.org"
-#  config.gem 'bullet', :source => 'http://gemcutter.org'
   config.gem 'validation_reflection', :source => 'http://gemcutter.org'
   
   
@@ -61,16 +59,7 @@ end
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   
-#  config.after_initialize do
-#    Bullet.enable = true 
-#    Bullet.alert = true
-#    Bullet.bullet_logger = true  
-#    Bullet.console = true
-#    Bullet.growl = true
-#    Bullet.rails_logger = true
-#    Bullet.disable_browser_cache = true
-#  end
-  
+
 end
 
 
