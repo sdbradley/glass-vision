a97ecdb75adf228ff861f128ae632c999f2c79b3
Jeff Paquette <jeff@snowmoonsoftware.com>
2013-03-25 01:21:52 +0000

    Set unit_price default to 0.0

--- db/migrate/20130325011604_add_default_for_unit_price.rb
+++ db/migrate/20130325011604_add_default_for_unit_price.rb
@@ -1,6 +1,6 @@
 class AddDefaultForUnitPrice < ActiveRecord::Migration
   def self.up
-    change_column :manual_lines, :unit_price, :float, :default => 0
+    change_column :manual_lines, :unit_price, :float, :default => 0.0
   end
 
   def self.down
