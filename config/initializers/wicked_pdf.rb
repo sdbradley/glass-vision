d6fed14084b73c8a49aea173c2524ce190114c8a
Jeff Paquette <jeff@snowmoonsoftware.com>
2013-04-24 16:13:53 +0000

    set path for production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,3 @@
 WickedPdf.config = {
-  :exe_path => '/Users/Jeff/.rvm/gems/ruby-1.8.7-p352@glassvision/bin/wkhtmltopdf'
+  :exe_path => '/home/glass/apps/inline/shared/bundle/ruby/1.8/gems/wkhtmltopdf-binary-0.9.9.1/bin/wkhtmltopdf'
 }
