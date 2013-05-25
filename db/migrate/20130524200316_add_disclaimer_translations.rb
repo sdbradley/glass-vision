01493a28bce18f3b18e7015176f2fb3c979e5206
Jeff Paquette <jeff@snowmoonsoftware.com>
2013-05-25 02:46:27 +0000

    add legalese to signature block

---
+++ db/migrate/20130524200316_add_disclaimer_translations.rb
@@ -0,0 +1,12 @@
+class AddDisclaimerTranslations < ActiveRecord::Migration
+  DATAFILE = __FILE__.gsub('.rb', '.sql')
+  def self.up
+    IO.readlines(DATAFILE).join.gsub("\r\n", "\n").split(";\n").each do |s|
+      execute(s) unless s == "\n"
+    end
+  end
+
+  def self.down
+    execute 'delete from translations where id >= 633;'
+    end
+end
