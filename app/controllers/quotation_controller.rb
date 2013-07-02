c5e967e7b28abc9d47de4716fa059487aad3dc3b
Jeff Paquette <jeff@snowmoonsoftware.com>
2013-07-02 18:08:19 +0000

    autocomplete now updates phone, fax and email

--- app/controllers/quotation_controller.rb
+++ app/controllers/quotation_controller.rb
@@ -1,6 +1,6 @@
 require 'retryable'
 class QuotationController < ApplicationController
-  autocomplete :customer, :name, :full => true, :extra_data => [:address]
+  autocomplete :customer, :name, :full => true, :extra_data => [:address, :phone, :fax, :email]
 
   before_filter :find_quotation, :only => [:show, :print, :print_invoice, :print_manifest, :print_calculations]
   sortable_attributes  :updated_at, :slug, :description, :user_id, :consultant
