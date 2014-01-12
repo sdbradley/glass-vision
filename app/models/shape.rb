78f609478c3488e3643f2d51423c86f79c6fb69c
Jeff Paquette <jeff@snowmoonsoftware.com>
2014-01-12 04:50:00 +0000

    trim whitespace from shape images on upload. Added new images for the rest of the shapes

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -9,9 +9,16 @@ class Shape < ActiveRecord::Base
                     },
                     :url => '/system/:class/:attachment/:id/:style_:basename.:extension',
                     :path => ':rails_root/public/system/:class/:attachment/:id/:style_:basename.:extension',
-                    :default_url => '/images/shapes/missing_:style.png'
+                    :default_url => '/images/shapes/missing_:style.png',
+                    :convert_options => { :all => '-trim' }
 
   validates_presence_of :name, :sections_width, :sections_height, :corners
   validates_numericality_of :sections_width, :sections_height
+#  validates_attachment_presence :photo
+#  validates_attachment_content_type :photo, :content_type => %w(image/jpg image/gif image/png),
+#                                    :message => 'file must be of filetype png, jpg or gif'
+
   has_and_belongs_to_many :series, :class_name => 'Serie'
+
+
 end
