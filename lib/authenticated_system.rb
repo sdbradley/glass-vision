8bae84e0424e0737f59303ded27cc05736927471
Jeff Paquette <jeff@snowmoonsoftware.com>
2014-05-02 02:24:50 +0000

    use request_url instead of request_uri ( rails change)

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -127,7 +127,7 @@ module AuthenticatedSystem
     #
     # We can return to this location by calling #redirect_back_or_default.
     def store_location
-      session[:return_to] = request.request_uri
+      session[:return_to] = request.url #use request_uri for rails < 3.1
     end
 
     # added
