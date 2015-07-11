032fdb2a46b803249af4d55bf38969f7b1ae37f4
Jeff Paquette <jeff@snowmoonsoftware.com>
2015-07-10 22:39:21 -0400

    Add translations for module type

---
+++ db/migrate/20150711015340_add_module_type_translations.rb
@@ -0,0 +1,24 @@
+class AddModuleTypeTranslations < ActiveRecord::Migration
+  def self.up
+    m = ModuleType.find(1)
+    m.translations.destroy_all
+    m.translations.create(:name => 'Window', :locale => :en)
+    m.translations.create(:name => 'Fenêtre', :locale => :fr)
+    m.translations.create(:name => 'Ventana', :locale => :es)
+    m.update_attributes(:name => 'Window')
+
+    m = ModuleType.find(2)
+    m.translations.destroy_all
+    m.translations.create(:name => 'Door', :locale => :en)
+    m.translations.create(:name => 'Porte', :locale => :fr)
+    m.translations.create(:name => 'Puerta', :locale => :es)
+    m.update_attributes(:name => 'Door')
+
+  end
+
+  def self.down
+    ModuleType.all.each do |m|
+      m.translations.destroy_all
+    end
+  end
+end
