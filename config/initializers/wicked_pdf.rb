7b103bff4fe1e24a8dd134d229574b7598d13ab1
Jeff Paquette <jeff@snowmoonsoftware.com>
2016-03-07 08:48:46 -0500

    added conditional path for development mode

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,10 @@
 WickedPdf.config = {
   :exe_path => '/usr/local/bin/wkhtmltopdf'
 }
+
+# override for development
+if Rails.env.development?
+  WickedPdf.config = {
+      :exe_path => '/Users/Jeff/snowmoon/glass-vision.net/vendor/bundle/gems/wkhtmltopdf-binary-0.9.9.3/bin/wkhtmltopdf'
+  }
+end
