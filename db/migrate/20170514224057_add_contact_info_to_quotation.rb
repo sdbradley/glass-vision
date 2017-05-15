c23d8b72bdb0aa4a01c11761a5b760db5ef77185
Jeff Paquette <jeff@snowmoonsoftware.com>
2017-05-15 18:57:09 -0400

    Added contact and cellphone to quotation. Add/Edit/Show working. Printing needs to be updated

---
+++ db/migrate/20170514224057_add_contact_info_to_quotation.rb
@@ -0,0 +1,22 @@
+#encoding: utf-8
+class AddContactInfoToQuotation < ActiveRecord::Migration
+  def self.up
+    add_column :quotations, :contact, :string, null:true
+    add_column :quotations, :cell_phone, :string, null:true
+
+    #    Translation.create(:translation_key =>'', :en =>'', :fr => '', :es =>'')
+    Translation.create(:translation_key =>'LABEL_QUOTATION_CONTACT', :en =>'Contact', :fr => 'Contact', :es =>'Contacto')
+    Translation.create(:translation_key =>'LABEL_QUOTATION_CELLPHONE', :en =>'Cell Phone', :fr => 'Téléphone cellulaire', :es =>'Teléfono móvil')
+
+
+  end
+
+  def self.down
+    remove_column :quotations, :cell_phone
+    remove_column :quotations, :contact
+
+    Translation.find_by_translation_key('LABEL_QUOTATION_CONTACT').destroy
+    Translation.find_by_translation_key('LABEL_QUOTATION_CELLPHONE').destroy
+
+  end
+end
