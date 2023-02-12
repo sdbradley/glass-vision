8d1a82acc486a76a1b71737c6266daf0376d5ad0
Scott Bradley <scott.bradley@daveramsey.com>
2023-02-12 13:05:40 -0600

    [GVB-9] enable stimuls and delete customers

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+import "@hotwired/turbo-rails"
+import './controllers'
+
+console.log('application.js');
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 pin "application", preload: true
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
