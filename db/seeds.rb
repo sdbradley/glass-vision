c0e8376ea8dc3fcae8b5743b6a5e720292cfd1a8
Ethan Dowler <ethan.dowler@daveramsey.com>
2023-03-07 21:38:24 -0600

    add address to glass vision company seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-glass_vision_co = Company.find_or_create_by!(name: "Glass-Vision")
+glass_vision_co = Company.find_or_create_by!(name: "Glass Vision")
+glass_vision_co.update!(
+  address: "123 Glassy Drive",
+  phone: "555-555-5555",
+  fax: "555-555-5555"
+)
 
 user = User.find_or_initialize_by(login: "root")
 unless user.persisted?
