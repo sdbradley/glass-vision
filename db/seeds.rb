5b139735c6b78d715ccaf41046b927d5e6fc8653
Ethan Dowler <ethan.dowler@daveramsey.com>
2023-03-11 13:44:12 -0600

    add admin user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Role.find_or_create_by!(rolename: "administrator")
 
 glass_vision_co = Company.find_or_create_by!(name: 'Glass Vision')
 glass_vision_co.update!(
@@ -13,6 +7,7 @@ glass_vision_co.update!(
   fax: '555-555-5555'
 )
 
+# non-admin user
 user = User.find_or_initialize_by(login: 'root')
 unless user.persisted?
   user.update!(
@@ -23,11 +18,33 @@ unless user.persisted?
   )
 end
 
-customer = Customer.find_or_create_by!(name: 'Win Dow')
-customer.update!(
-  user: user,
-  address: '123 Drafty Way',
-  phone: '555-555-5555',
-  fax: '555-555-5555',
-  email: 'win.dow@example.com'
-)
+Customer
+  .find_or_create_by!(name: 'Win Dow')
+  .update!(
+    user: user,
+    address: '123 Drafty Way',
+    phone: '555-555-5555',
+    fax: '555-555-5555',
+    email: 'win.dow@example.com'
+  )
+
+# admin user
+admin = User.find_or_initialize_by(login: 'admin')
+unless admin.persisted?
+  admin.update!(
+    email: 'admin@example.com',
+    password: 'PASSWORD',
+    password_confirmation: 'PASSWORD',
+    activated_at: Time.current
+  )
+end
+
+Customer
+  .find_or_create_by!(name: 'Wind Shield')
+  .update!(
+    user: admin,
+    address: '123 Breezy Way',
+    phone: '555-555-5555',
+    fax: '555-555-5555',
+    email: 'wind.shield@example.com'
+  )
