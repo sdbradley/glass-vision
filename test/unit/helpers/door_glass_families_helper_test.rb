9ed072a8caef63c3d68ff1bec7d9e4cb86680c46
Ethan Dowler <ethan.dowler@daveramsey.com>
2023-03-11 10:29:13 -0600

    remove unused test files

--- test/unit/helpers/door_glass_families_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DoorGlassFamiliesHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/product_colors_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ProductColorsHelperTest < ActionView::TestCase
-end
