2ce5b21fc6fccc5ce53918f80ad61a2fb144804f
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 11:36:20 -0500

    companies_users not exists

--- db/migrate/1032_create_companies_users.rb
+++ db/migrate/1032_create_companies_users.rb
@@ -1,12 +1,12 @@
 class CreateCompaniesUsers < ActiveRecord::Migration[7.0]
   def self.up
-     create_table :companies_users, :id => false do |t|
+     create_table :companies_users, if_not_exists: true do |t|
           t.column :company_id, :integer
           t.column :user_id, :integer
       end
     end
     
   def self.down
-    drop_table :companies_users
+    drop_table :companies_users, if_exists: true
   end
 end
