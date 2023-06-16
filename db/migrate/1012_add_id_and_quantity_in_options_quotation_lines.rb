1abefd5f83eafc8f4e3f8856eb451b568723048d
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 10:04:08 -0500

    auto increment error

--- db/migrate/1012_add_id_and_quantity_in_options_quotation_lines.rb
+++ db/migrate/1012_add_id_and_quantity_in_options_quotation_lines.rb
@@ -1,7 +1,7 @@
 class AddIdAndQuantityInOptionsQuotationLines < ActiveRecord::Migration[7.0]
   def self.up
     execute "ALTER TABLE options_quotation_lines
-             ADD COLUMN id INTEGER  NOT NULL AUTO_INCREMENT AFTER quotation_line_id,
+             ADD COLUMN id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY AFTER quotation_line_id,
              ADD COLUMN quantity INTEGER  NOT NULL DEFAULT 1 AFTER id,
              ADD PRIMARY KEY (id)"
   end
