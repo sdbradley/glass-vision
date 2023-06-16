6c88ba1ae165095cb8e5489a7c4f25fc00827646
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 14:34:12 -0500

    AddFrameProfileToDoorLine

--- db/migrate/20111012014403_add_frame_profile_to_door_line.rb
+++ db/migrate/20111012014403_add_frame_profile_to_door_line.rb
@@ -1,9 +1,9 @@
 class AddFrameProfileToDoorLine < ActiveRecord::Migration[7.0]
   def self.up
-    add_column :door_lines, :frame_profile_id, :integer
+    add_column :door_lines, :frame_profile_id, :integer, if_not_exists: true
   end
 
   def self.down
-    remove_column :door_lines, :frame_profile_id
+    remove_column :door_lines, :frame_profile_id, if_exists: true
   end
 end
