c6a540f7f38688018f94d12a9a7f4b770ad73963
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 11:17:30 -0500

    fixed SetUnitPriceQuantifiable migration

--- db/migrate/1014_set_unit_price_quantifiable.rb
+++ db/migrate/1014_set_unit_price_quantifiable.rb
@@ -1,13 +1,13 @@
 class SetUnitPriceQuantifiable < ActiveRecord::Migration[7.0]
   def self.up
     execute "UPDATE pricing_methods
-             SET quantifiable = 1
+             SET quantifiable = true
              WHERE id = 6"
   end
 
   def self.down
   	execute "UPDATE pricing_methods
-             SET quantifiable = 0
+             SET quantifiable = false
              WHERE id = 6"
   end
 end
