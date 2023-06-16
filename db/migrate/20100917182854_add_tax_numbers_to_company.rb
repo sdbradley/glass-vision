96bf3db750a599d2745261ef29cac1eaadd99561
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 12:45:00 -0500

    AddTaxNumbersToCompany

--- db/migrate/20100917182854_add_tax_numbers_to_company.rb
+++ db/migrate/20100917182854_add_tax_numbers_to_company.rb
@@ -2,8 +2,8 @@ class AddTaxNumbersToCompany < ActiveRecord::Migration[7.0]
   DATAFILE = __FILE__.gsub('.rb', '.sql')
 
   def self.up
-    add_column :companies, :gst_number, :string
-    add_column :companies, :pst_number, :string
+    add_column :companies, :gst_number, :string, if_not_exists: true
+    add_column :companies, :pst_number, :string, if_not_exists: true
 
     IO.readlines(DATAFILE).join.gsub("\r\n", "\n").split(";\n").each do |s|
       execute(s) unless s == "\n"
@@ -12,8 +12,8 @@ class AddTaxNumbersToCompany < ActiveRecord::Migration[7.0]
   end
 
   def self.down
-    remove_column :companies, :pst_number
-    remove_column :companies, :gst_number
+    remove_column :companies, :pst_number, if_exists: true
+    remove_column :companies, :gst_number, if_exists: true
     execute 'delete from translations where id >= 494;'
   end
 end
