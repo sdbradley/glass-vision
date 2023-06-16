5e1125efac80c435084e987f054eb8f09bddac42
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 15:14:25 -0500

    AddSlabMaterialToDoorLine

--- db/migrate/20111017001149_add_slab_material_to_door_line.rb
+++ db/migrate/20111017001149_add_slab_material_to_door_line.rb
@@ -1,9 +1,9 @@
 class AddSlabMaterialToDoorLine < ActiveRecord::Migration[7.0]
   def self.up
-    add_column :door_lines, :slab_material_id, :integer
+    add_column :door_lines, :slab_material_id, :integer, if_not_exists: true
   end
 
   def self.down
-    remove_column :door_lines, :slab_material_id
+    remove_column :door_lines, :slab_material_id, if_exists: true
   end
 end
