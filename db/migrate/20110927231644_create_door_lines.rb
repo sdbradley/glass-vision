37bebf0139bf647a2c659d3dbcd711ee4cb62986
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 14:08:05 -0500

    door_lines

--- db/migrate/20110927231644_create_door_lines.rb
+++ db/migrate/20110927231644_create_door_lines.rb
@@ -1,6 +1,6 @@
 class CreateDoorLines < ActiveRecord::Migration[7.0]
   def self.up
-    create_table :door_lines do |t|
+    create_table :door_lines, if_not_exists: true do |t|
       t.integer :quotation_id
       t.integer :door_frame_id
       t.integer :quantity
