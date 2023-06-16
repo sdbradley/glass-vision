e89446a3d7b7cafa063f425b053e41af4337eba0
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 09:43:19 -0500

    if_not_exists

--- db/migrate/1009_add_discount_to_user.rb
+++ db/migrate/1009_add_discount_to_user.rb
@@ -1,6 +1,6 @@
 class AddDiscountToUser < ActiveRecord::Migration[7.0]
   def self.up
-    add_column :users, :discount, :float, :default => 0.0, :null => true
+    add_column :users, :discount, :float, :default => 0.0, :null => true, if_not_exists: true
     # update existing rows here
     User.reset_column_information
     User.update_all("discount = 0.0")
@@ -8,7 +8,7 @@ class AddDiscountToUser < ActiveRecord::Migration[7.0]
   end
 
   def self.down
-  	remove_column :users, :discount
+  	remove_column :users, :discount, if_exists: true
   end
 
 end
