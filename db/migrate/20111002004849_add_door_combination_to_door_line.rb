6e076d22bf3daba659e22af338f31813bb0338f6
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 14:21:54 -0500

    AddDoorCombinationToDoorLine

--- db/migrate/20111002004849_add_door_combination_to_door_line.rb
+++ db/migrate/20111002004849_add_door_combination_to_door_line.rb
@@ -1,9 +1,9 @@
 class AddDoorCombinationToDoorLine < ActiveRecord::Migration[7.0]
   def self.up
-    add_column :door_lines, :door_combination_id, :integer
+    add_column :door_lines, :door_combination_id, :integer, if_not_exists: true
   end
 
   def self.down
-    remove_column :door_lines, :door_combination_id
+    remove_column :door_lines, :door_combination_id, if_exists: true
   end
 end
