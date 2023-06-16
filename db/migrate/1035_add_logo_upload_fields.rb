5b2f3c61c2160a663624bf9d477b76b8c290ea2c
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 12:14:44 -0500

    AddLogoUploadFields3

--- db/migrate/1035_add_logo_upload_fields.rb
+++ db/migrate/1035_add_logo_upload_fields.rb
@@ -1,6 +1,8 @@
 class AddLogoUploadFields < ActiveRecord::Migration[7.0]
   def self.up
-    rename_column :companies, :logo, :logo_file_name, if_exists: true
+    if Event.attribute_names.include? "logo"
+      rename_column :companies, :logo, :logo_file_name
+    end
     add_column :companies, :logo_content_type, :string, if_not_exists: true # Mime type
     add_column :companies, :logo_file_size, :integer, if_not_exists: true # File size in bytes
   end
