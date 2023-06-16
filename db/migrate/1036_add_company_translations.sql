1a3e3db384e44f6abba3d3897a4b33b3174e67aa
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 12:30:16 -0500

    1036_add_company_translations

--- db/migrate/1036_add_company_translations.sql
+++ db/migrate/1036_add_company_translations.sql
@@ -1,4 +1,4 @@
-INSERT INTO translations VALUES (440, 'TITLE_COMPANY_ADD_USERS', 'Ajouter des utilisateurs n l''entreprise','','Add Users To Company', 'A�ada a los utilizadores a la companga');
+INSERT INTO translations VALUES (440, 'TITLE_COMPANY_ADD_USERS', 'Ajouter des utilisateurs n l''entreprise','','Add Users To Company', 'Anada a los utilizadores a la companga');
 INSERT INTO translations VALUES (441, 'TITLE_COMPANY_ACTIVE_USERS', 'utilisateurs actifs','','Active Users', 'usuarios activos');
 INSERT INTO translations VALUES (442, 'BUTTON_COMPANY_EDIT_USERS', 'utilisateurs','','Users', 'Usuarios');
 INSERT INTO translations VALUES (443, 'LABEL_ES', 'Espagnol','','Spanish', 'Espanol');
