8b63f828411c21017154807f38f1b955803c3afb
Scott Bradley <scott.bradley@daveramsey.com>
2023-06-16 15:03:09 -0500

    AddPriceToSlabMaterials

--- db/migrate/20120228013710_add_price_to_slab_materials.rb
+++ db/migrate/20120228013710_add_price_to_slab_materials.rb
@@ -1,9 +1,9 @@
 class AddPriceToSlabMaterials < ActiveRecord::Migration[7.0]
   def self.up
-    add_column :slab_materials, :price, :float, :default => 0.0
+    add_column :slab_materials, :price, :float, :default => 0.0, if_not_exists: true
   end
 
   def self.down
-    remove_column :slab_materials, :price
+    remove_column :slab_materials, :price, if_exists: true
   end
 end
